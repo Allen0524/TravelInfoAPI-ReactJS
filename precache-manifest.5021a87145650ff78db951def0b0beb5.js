ce195c455fe3d3c1d0afaa85d7a74625cbf10008
Allen0524 <william0318563@gmail.com>
2020-09-04 11:32:53 +0800

    Updates

---
+++ precache-manifest.5021a87145650ff78db951def0b0beb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "496a3b67486b0daf444c784bac7eb6d6",
+    "url": "/TravelInfoAPI-ReactJS/index.html"
+  },
+  {
+    "revision": "4c9871e3c1127f297f6e",
+    "url": "/TravelInfoAPI-ReactJS/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9f2b70a09851f3fcc13a",
+    "url": "/TravelInfoAPI-ReactJS/static/css/main.44b569af.chunk.css"
+  },
+  {
+    "revision": "4c9871e3c1127f297f6e",
+    "url": "/TravelInfoAPI-ReactJS/static/js/2.3136b3f8.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/TravelInfoAPI-ReactJS/static/js/2.3136b3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2b70a09851f3fcc13a",
+    "url": "/TravelInfoAPI-ReactJS/static/js/main.e34b7bcc.chunk.js"
+  },
+  {
+    "revision": "a2e51b3c5c31e6351090",
+    "url": "/TravelInfoAPI-ReactJS/static/js/runtime-main.d374f369.js"
+  },
+  {
+    "revision": "87426b414f411d13aa36f7576c446f72",
+    "url": "/TravelInfoAPI-ReactJS/static/media/cover.87426b41.jpg"
+  }
+]);
